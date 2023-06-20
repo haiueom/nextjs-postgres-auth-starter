9aa8f429fd8dd33a1ee53bdef418733698de792e
haiueom <ilhamtaufiq859@gmail.com>
2023-06-20 21:45:13 +0700

    update

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -3,14 +3,6 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
-    const { id } = await req.json();
-    if (id) {
-        const product = await prisma.product.findUnique({
-            where: { id: Number(id) },
-        });
-        
-        return NextResponse.json(product);
-    }
     const products = await prisma.product.findMany({
         orderBy: { createdAt: "desc" },
     });
