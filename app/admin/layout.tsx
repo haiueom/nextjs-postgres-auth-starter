c6363678a21669856778ff16d4ea4fccf1090905
haiueom <ilhamtaufiq859@gmail.com>
2023-06-25 20:20:39 +0700

    update layout

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,25 +1,15 @@
-import '@/styles/globals.css';
-
 import Navbar from '@/components/navbars/navbar';
 import { Suspense } from 'react';
 import { getServerSession } from 'next-auth/next';
 
-export const metadata = {
-    title: 'Dashboard Admin',
-    description:
-        'A user admin dashboard configured with Next.js, PlanetScale, NextAuth, Tailwind CSS, TypeScript, ESLint, and Prettier.',
-};
-
-export default async function RootLayout({ children }: { children: React.ReactNode }) {
+export default async function AdminLayout({ children }: { children: React.ReactNode }) {
     const session = await getServerSession();
     return (
-        <html lang="en" className="h-full bg-gray-50">
-            <body className="h-full">
-                <Suspense>
-                    <Navbar user={session?.user} />
-                </Suspense>
-                {children}
-            </body>
-        </html>
-    );
+        <>
+            <Suspense>
+                <Navbar user={session?.user} />
+            </Suspense>
+            {children}
+        </>
+    )
 }
